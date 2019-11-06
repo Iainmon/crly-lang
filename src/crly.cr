36c99ddd0111815e77cb33f881949945350ea96b
Iain Moncrief <Iainmoncrief@gmail.com>
2019-11-05 20:03:55 -1000

    made compiler changes

--- src/crly.cr
+++ src/crly.cr
@@ -8,33 +8,61 @@ module Crly
     "std" => true
   }
 
-  def self.compile_file(filename : String, options = DEFAULT_COMPILER_OPTIONS)
+  def self.compile_file(filename : String, options = DEFAULT_COMPILER_OPTIONS) : String
 
     raise "Error opening file! The file '#{filename}' does not exist." unless File.exists?(filename)
 
     contents = File.read(filename)
-  
+
     processor = Processor.new filename
-  
+
     contents = processor.process(contents)
-  
+
     outputFilename = ARGV[1] if ARGV.size > 1
-  
+
     File.write(outputFilename || filename.gsub(".soph", ".cr").underscore, contents)
 
+    return
   end
 
 
   Phreak.parse! do |root|
-    root.default do
-      puts "No file provided."
+
+
+
+    root.bind(word: "build") do
+      root.unrecognized_args do |arg|
+        Crly.compile_file(arg)
+      end
+
+      root.default do
+        puts "No file provided."
+      end
     end
-  
+
+    root.bind(word: "run") do
+
+      root.unrecognized_args do |arg|
+        Crly.compile_file(arg)
+
+        Process.exec("crystal", [arg])
+      end
+
+      root.default do
+        puts "No file provided."
+      end
+    end
+
     root.unrecognized_args do |arg|
       Crly.compile_file(arg)
     end
+
+    root.default do
+      puts "No file provided."
+    end
+
   end
 
 
-  
+
 end
